fe8b5c982d85c139aeddaabfe480c2e2d88160c1
Valikmeister <v.kostuykovskiy@gmail.com>
2024-04-16 18:49:59 +0200

    Project Structure

---
+++ src/backend/infrastructure/driven/port/database/shared/migrations/create_table_authors.sql
@@ -0,0 +1,9 @@
+CREATE TABLE authors (
+    AuthorId int NOT NULL AUTO_INCREMENT,
+    FirstName varchar(255) NOT NULL,
+    LastName varchar(255) NOT NULL,
+    Description varchar(255),
+    
+    PRIMARY KEY(AuthorId)
+
+)
